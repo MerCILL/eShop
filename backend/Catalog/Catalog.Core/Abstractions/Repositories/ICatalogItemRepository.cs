c71487512033136e186683555be7adbc0e69fbe8
merciq <ted270503@gmail.com>
2024-01-26 03:30:12 +0200

    Deleted old folder

--- backend/Catalog/Catalog.Core/Abstractions/Repositories/ICatalogItemRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Catalog.Core.Abstractions.Repositories;
-
-public interface ICatalogItemRepository<CatalogItemEntity> : IRepository<CatalogItemEntity> where CatalogItemEntity : class
-{
-    Task<CatalogItemEntity> GetByTitle(string title);
-    Task<CatalogItemEntity> GetByPictureFile(string pictureFile);
-}
--- backend/Catalog/Catalog.Core/GlobalUsings.cs
+++
@@ -1,2 +0,0 @@
-﻿global using Catalog.Domain.Models;
-
