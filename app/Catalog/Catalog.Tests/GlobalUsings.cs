f148a53fe4b3d2d6eced3cea4381609a31d45217
merciq <ted270503@gmail.com>
2024-02-12 20:33:38 +0200

    Added tests in Catalog API

---
+++ app/Catalog/Catalog.Tests/GlobalUsings.cs
@@ -0,0 +1,12 @@
+//Catalog
+global using Catalog.Application.Services;
+global using Catalog.Core.Abstractions.Repositories;
+global using Catalog.DataAccess.Entities;
+global using Catalog.Application.Infrastructure.Exceptions;
+global using Catalog.Domain.Models;
+
+//Packages
+global using Xunit;
+global using Microsoft.Extensions.Logging;
+global using Moq;
+global using AutoMapper;
